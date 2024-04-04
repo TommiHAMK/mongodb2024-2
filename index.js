03c787d1a172c394bece8c7d1120d1e197470464
aatosmedia <tommi.saksa@aatosmedia.fi>
2024-04-04 09:10:55 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
+require('dotenv').config();
+
+const app = express();
+
+// default settings for Handlebars
+app.engine('handlebars', exphbs.engine({
+    defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+const dbURI = 'mongodb+srv://'+process.env.DBUSERNAME+':'
++process.env.DBPASSWORD+'@'+process.env.CLUSTER+'.mongodb.net/'
++process.env.DB+'?retryWrites=true&w=majority';
+
+//console.log(dbURI);
+
+mongoose.connect(dbURI)
+.then((result) => 
+{
+    console.log('Connected to DB');
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log("Listening on " + PORT));
+})
+.catch((err) => {
+    console.log(err);
+})
+
+const Product = require('./models/Product');
+
+/*
+const newProduct = new Product({
+    name: 'Chair',
+    price: 100
+});
+
+newProduct.save()
+.then((result) => 
+{
+    console.log(result);
+})
+.catch((err) => {
+    console.log(err);
+})
+*/
+/*
+Product.find()
+.then((result) => {
+    console.log(result);
+})
+*/
+
+const getAll = async () => {
+    try
+    {
+        const result = await Product.find();
+        console.log(result);
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
+}
+
+//getAll();
+
+
+app.get('/', (req,res) => {
+    res.render('index');
+})
+
+app.get('/products', async (req,res) => {
+    try {
+        const result = await Product.find();
+        res.json(result);
+    }
+    catch (error) {
+        console.log(error);
+    }
+})
+
+
+app.get('/products/:id', async (req,res) => {
+    const id = req.params.id;
+    const product = await Product.findById(id);
+    res.json(product);
+});
+
+
+
+
